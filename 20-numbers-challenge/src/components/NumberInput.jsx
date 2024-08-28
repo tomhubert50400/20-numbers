27ea33f4d77757bf5f247ea7464f892abe56684a
tomhubert50400 <73112644+tomhubert50400@users.noreply.github.com>
2024-08-28 22:51:30 +0200

    number input component

---
+++ 20-numbers-challenge/src/components/NumberInput.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NumberInput({ number }) {
+  return (
+    <div className="number-input">
+      <h2>Current Number: {number}</h2>
+    </div>
+  );
+}
+
+export default NumberInput;
